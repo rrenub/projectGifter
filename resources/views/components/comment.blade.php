f332b93392b4b089f2d1efcfed0236f9f52470ca
pedro gil <pederitoo@gmail.com>
2021-01-16 18:08:48 +0000

    edited comment and valoration

--- resources/views/components/comment.blade.php
+++ resources/views/components/comment.blade.php
@@ -1,11 +1,20 @@
     <div class=" shadow-md px-4 py-3 rounded-lg w-full">
         <div class="flex items-center">
-            <img class="h-12 w-12 rounded-full" src="https://lh3.googleusercontent.com/proxy/plfa3Sn3t86C90C9AUtiwC7_AkqdCk1JpxDI0WeAKK9G1ffDRguMFAJ4gd76q0jfuT4kj47xm4jlR2UXOJlNnhzczCklWknEBPdi8IOgaIyFG5ZZvj07"/>
+            <img class="h-16 w-16" src="https://lh3.googleusercontent.com/proxy/plfa3Sn3t86C90C9AUtiwC7_AkqdCk1JpxDI0WeAKK9G1ffDRguMFAJ4gd76q0jfuT4kj47xm4jlR2UXOJlNnhzczCklWknEBPdi8IOgaIyFG5ZZvj07"/>
             <div class="ml-2">
-                <div class="text-sm ">
-                    <span class="font-semibold">Dallin Baumbach</span>
+                <div class="text-sm">
+                    <span class="font-semibold text-lg">Dallin Baumbach</span>
                 </div>
-                <div class="text-gray-500 text-xs flex">
+                <div class="">
+                    <div class="flex items-center">
+                        <svg class="w-3 h-3 mt-1 fill-current text-yellow-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M10 15l-5.878 3.09 1.123-6.545L.489 6.91l6.572-.955L10 0l2.939 5.955 6.572.955-4.756 4.635 1.123 6.545z"/></svg>
+                        <svg class="w-3 h-3 mt-1 fill-current text-yellow-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M10 15l-5.878 3.09 1.123-6.545L.489 6.91l6.572-.955L10 0l2.939 5.955 6.572.955-4.756 4.635 1.123 6.545z"/></svg>
+                        <svg class="w-3 h-3 mt-1 fill-current text-yellow-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M10 15l-5.878 3.09 1.123-6.545L.489 6.91l6.572-.955L10 0l2.939 5.955 6.572.955-4.756 4.635 1.123 6.545z"/></svg>
+                        <svg class="w-3 h-3 mt-1 fill-current text-yellow-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M10 15l-5.878 3.09 1.123-6.545L.489 6.91l6.572-.955L10 0l2.939 5.955 6.572.955-4.756 4.635 1.123 6.545z"/></svg>
+                        <svg class="w-3 h-3 mt-1 fill-current text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M10 15l-5.878 3.09 1.123-6.545L.489 6.91l6.572-.955L10 0l2.939 5.955 6.572.955-4.756 4.635 1.123 6.545z"/></svg>
+                    </div>
+                </div>
+                <div class="text-gray-500 text-xs">
                     <svg class="inline-block ml-1 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" data-supported-dps="16x16" fill="currentColor" class="mercado-match" width="16" height="16" focusable="false">
                         <path d="M8 1a7 7 0 107 7 7 7 0 00-7-7zM3 8a5 5 0 011-3l.55.55A1.5 1.5 0 015 6.62v1.07a.75.75 0 00.22.53l.56.56a.75.75 0 00.53.22H7v.69a.75.75 0 00.22.53l.56.56a.75.75 0 01.22.53V13a5 5 0 01-5-5zm6.24 4.83l2-2.46a.75.75 0 00.09-.8l-.58-1.16A.76.76 0 0010 8H7v-.19a.51.51 0 01.28-.45l.38-.19a.74.74 0 01.68 0L9 7.5l.38-.7a1 1 0 00.12-.48v-.85a.78.78 0 01.21-.53l1.07-1.09a5 5 0 01-1.54 9z"></path>
                     </svg>
@@ -13,7 +22,7 @@
                 </div>
             </div>
         </div>
-        <p class="text-gray-800 text-sm mt-2 leading-normal md:leading-relaxed">Aqui ira el comentario que hayan escrito los usuarios, como se puede ver cuando se escribe un texto largo va pa bajo sabe lo k t digo?</p>
+        <p class="text-gray-800 text-md mt-2 leading-normal md:leading-relaxed">Aqui ira el comentario que hayan escrito los usuarios, como se puede ver cuando se escribe un texto largo va pa bajo sabe lo k t digo?</p>
         <div class="text-gray-500 text-xs flex items-center mt-3">
             <img class="mr-0.5" src="https://static-exp1.licdn.com/sc/h/d310t2g24pvdy4pt1jkedo4yb"/>
             <img class="mr-0.5" src="https://static-exp1.licdn.com/sc/h/5thsbmikm6a8uov24ygwd914f"/>

b194bb34f9f2b4f6b5b5f1624ec1c2df8c3a56bc
Ruben Delgado <cuenta.rubendelgado@gmail.com>
2021-01-16 18:29:02 +0000

    added href to each product to go to detail

--- resources/views/components/card-item.blade.php
+++ resources/views/components/card-item.blade.php
@@ -1,11 +1,11 @@
 <div>
     <div class="max-w-xs bg-white shadow-lg rounded-lg overflow-hidden my-2 mx-2">
-        <a href="#">
+        <a href='/detalleProducto?id={{ $idProd }}'>
             <img class="h-40 w-auto object-cover my-4 mx-auto rounded-lg" src="{{ $img }}" alt="NIKE AIR">
         </a>
         <div class="px-4 py-2 bg-gray-800">
             <div class="mb-4">
-                <a href="#">
+                <a href='/detalleProducto?id={{ $idProd }}'>
                     <h1 class="text-white font-bold text-xl truncate hover:text-yellow-500 cursor-pointer">{{ $name }}</h1>
                 </a>
                 <p class="text-white text-sm mt-1 truncate">{{ $description }}</p>
